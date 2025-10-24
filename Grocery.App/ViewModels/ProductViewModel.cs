b06d4617b1d260013c6cedffa47af10f9995c99e
Johan <s1212230@student.windesheim.nl>
2025-10-24 18:42:27 +0200

    UC19 Product in database en nieuw product aanmaken is deels uitgewerkt. Alleen crasht het wel als ik op de knop "nieuw product" klikt.

--- Grocery.App/ViewModels/ProductViewModel.cs
+++ Grocery.App/ViewModels/ProductViewModel.cs
@@ -1,19 +1,51 @@
-﻿using Grocery.Core.Interfaces.Services;
+﻿﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Grocery.App.Views;
+using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
 using System.Collections.ObjectModel;
 
 namespace Grocery.App.ViewModels
 {
-    public class ProductViewModel : BaseViewModel
+    public partial class ProductViewModel : BaseViewModel
     {
         private readonly IProductService _productService;
         public ObservableCollection<Product> Products { get; set; }
+        [ObservableProperty]
+        Client client;
 
-        public ProductViewModel(IProductService productService)
+        public ProductViewModel(IProductService productService, GlobalViewModel global)
         {
             _productService = productService;
             Products = [];
+            LoadProducts();
+            client = global.Client;
+        }
+
+        private void LoadProducts()
+        {
+            Products.Clear();
             foreach (Product p in _productService.GetAll()) Products.Add(p);
+        }
+
+        // Deze methode wordt aangeroepen wanneer de pagina verschijnt
+        public void OnAppearingProduct() 
+        {
+            LoadProducts();
+        }
+
+        [RelayCommand]
+        public async Task ShowNewProduct()
+        {
+            if (Client.Role == Role.Admin)
+            {
+                // Navigeren naar de nieuwe productpagina
+                await Shell.Current.GoToAsync(nameof(NewProductView));
+            }
+            else
+            {
+                await Shell.Current.DisplayAlert("Toegang geweigerd", "Je hebt geen toestemming om een nieuw product toe te voegen.", "OK");
+            }
         }
     }
 }
